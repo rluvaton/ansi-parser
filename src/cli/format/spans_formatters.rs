53f506dce90404f0833595ceab2e2f1ef7df112d
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2024-05-26 17:55:34 +0300

    move formats

--- src/cli/format/spans_formatters.rs
+++
@@ -1,2 +0,0 @@
-pub mod json_formatter;
-pub mod single_item_json_formatter;
--- src/format/lines_formatters/flat_json_line_formatter.rs
+++ src/format/lines_formatters/flat_json_line_formatter.rs
@@ -1,13 +1,28 @@
 use std::iter::Iterator;
 
 use genawaiter::{rc::gen, yield_};
-
+use serde::{Deserialize, Serialize};
+use crate::parse_ansi_text::ansi::types::Span;
 use crate::parse_ansi_text::ansi_output_to_spans::parse_ansi_split_by_lines_as_spans::Line;
+use crate::traits::ToJson;
 
-pub fn json_lines_single_item_formatter<I: Iterator<Item=Line>>(iter: I) -> impl Iterator<Item = String> {
+// Just to have a struct for the type in the tests
+
+#[derive(Debug, PartialEq, Clone, Deserialize)]
+struct LineMarker {
+    #[serde(rename(serialize = "type_", deserialize = "type"))]
+    type_: String
+}
+
+const NEW_LINE_MARKER: &str = r#"{ "type": "new line" }"#;
+
+pub fn flat_json_line_formatter<I: Iterator<Item=Line>>(iter: I) -> impl Iterator<Item = String> {
     return gen!({
         for line in iter {
-            yield_!(sonic_rs::to_string(&line.spans).unwrap());
+            for span in line.spans.iter() {
+                yield_!(span.to_json());
+            }
+            yield_!(NEW_LINE_MARKER.to_string());
         }
     }).into_iter();
 
@@ -16,7 +31,6 @@ pub fn json_lines_single_item_formatter<I: Iterator<Item=Line>>(iter: I) -> impl
 #[cfg(test)]
 mod tests {
     use pretty_assertions::assert_eq;
-    use crate::cli::format::spans_formatters::single_item_json_formatter::json_single_item_formatter;
 
     use crate::parse_ansi_text::ansi::style::{Brightness, TextStyle};
     use crate::parse_ansi_text::ansi::types::Span;
@@ -25,7 +39,7 @@ mod tests {
 
 
     #[test]
-    fn test_formatter_each_item_is_valid_json_line() {
+    fn test_formatter_each_span_is_different_line_and_have_delimiter() {
         let lines: Vec<Line> = vec![
             Line {
                 location_in_file: 0,
@@ -55,25 +69,30 @@ mod tests {
             }
         ];
 
-        let outputs_iter = json_lines_single_item_formatter(lines.clone().into_iter());
-
-
-
         let spans_lines = lines.clone().iter().map(|line| line.spans.clone()).collect::<Vec<Vec<Span>>>();
+        let number_of_spans = spans_lines.iter().map(|spans| spans.len()).sum::<usize>();
+
+        let outputs_iter = flat_json_line_formatter(lines.clone().into_iter());
 
         let outputs: Vec<String> = outputs_iter.collect();
-
-        // parse each item
+        
+        assert_eq!(outputs.len(), number_of_spans + spans_lines.len());
+        
         let mut i = 0;
-        for output in outputs.iter() {
-            let output_spans = sonic_rs::from_str::<Vec<Span>>(output.as_str()).expect("Failed to parse json array");
 
-            assert_eq!(output_spans, spans_lines[i]);
+        for line in spans_lines {
+            for span in line {
+                let output_span = sonic_rs::from_str::<Span>(outputs[i].as_str()).expect("Failed to parse span");
+                
+                assert_eq!(output_span, span);
+                i += 1;
+            }
+            
+            let output_line_marker = sonic_rs::from_str::<LineMarker>(outputs[i].as_str()).expect("Failed to parse line marker");
 
+            assert_eq!(output_line_marker, LineMarker { type_: "new line".to_string() });
+            
             i += 1;
         }
-
-        assert_eq!(i, spans_lines.len());
-
     }
 }

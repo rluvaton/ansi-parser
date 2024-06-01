b392f6c1b1775f5459c621d4e865c0534ede5831
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2024-06-02 02:49:35 +0300

    fix run cli script

--- run_lib/src/cli/parse_command.rs
+++ run_lib/src/cli/parse_command.rs
@@ -21,21 +21,7 @@ pub fn run_parse_command(matches: &clap::ArgMatches) {
         .get_one::<String>("file")
         .expect("Should have been able to get the file path");
 
-    let output = matches
-        .get_one::<String>("output")
-        .expect("Should have been able to get the output destination");
-
     let input_file_path = PathBuf::from(OsString::from(file_path));
-
-    let format = matches
-        .get_one::<String>("format")
-        .expect("Should have been able to get the format");
-
-    let flat_json_line_output_format = format == "flat-json-line";
-
-    if !split_by_lines && flat_json_line_output_format {
-        panic!("'flat-json-line' option is only available when 'split-lines' is enabled");
-    }
 
     let middle_of_file_info =
         get_from_middle_of_the_file_info(input_file_path, from_line, to_line, mapping_file);
@@ -49,23 +35,18 @@ pub fn run_parse_command(matches: &clap::ArgMatches) {
     let parse_options = ParseOptions::default()
         .with_initial_span(middle_of_file_info.initial_span.unwrap_or(Span::empty()));
 
-    if !split_by_lines {
-        let options = ReadAnsiFileOptions {
-            file_options: file_reader_options,
-            parse_options,
-        };
+    let options = ReadAnsiFileOptions {
+        file_options: file_reader_options,
+        parse_options,
+    };
 
+    if !split_by_lines {
         let_gen_using!(spans_iterator, |co| read_ansi_file_to_spans_producer(options, co));
 
         spans_iterator.into_iter().for_each(|_| {
             // Noop
         });
     } else {
-        let options = ReadAnsiFileOptions {
-            file_options: file_reader_options,
-            parse_options,
-        };
-
         let_gen_using!(lines_iterator, |co| read_ansi_file_to_lines_producer(options, co));
 
         lines_iterator.into_iter().for_each(|_| {
